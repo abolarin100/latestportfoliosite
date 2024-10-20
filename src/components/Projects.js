730fa39f33fcbfca9224e6eee968706e45658c1d
abolarin100 <abolarin100@gmail.com>
2024-10-20 11:14:15 -0700

    updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -46,36 +46,7 @@ const Projects = () => {
 
                     </div>
 
-                    <div className='flex flex-col items-start space-y-[1px] bg-[#A7AEB91A] hover:scale-105'>
-                        <img src={Ha} alt=""
-                            className=' w-full object-cover'
-                        />
-                        <div className=' shadow-md shadow-gray-400 w-full rounded-b-md'>
-                            <div className='flex flex-row justify-between pt-2 px-4'>
-                                <div className='pt-2'>
-                                    <h2 className='font-semibold'>Jay's Menu </h2>
-                                    <div className='flex space-x-2 pt-2'>
-                                        <p> A comprehensive online menu for browsing dishes, exploring descriptions, and viewing visuals</p>
-
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div className='flex flex-row justify-between px-4 pt-4 pb-4'>
-                                <a href="https://github.com/abolarin100/food-menu">
-                                    <button className='hover:bg-[#7b8ca71a] hover:scale-125 text-xs bg-[#415f8f1a] text-blue-500 border-1 border-blue-500' >GitHub</button>
-                                </a>
-                                <a href="https://jays-menu.vercel.app/">
-                                    <button className='hover:bg-[#a5b3c91a] hover:scale-125 text-xs bg-[#415f8f1a] text-blue-500 border-1 border-blue-500' >Live link</button>
-
-                                </a>
-                            </div>
-
-                        </div>
-
-
-                    </div>
+                   
                     <div className='flex flex-col items-start space-y-[1px] bg-[#A7AEB91A] hover:scale-105'>
                         <img src={La} alt=""
                             className=' w-full object-cover'
@@ -137,6 +108,38 @@ const Projects = () => {
 
 
                     </div>
+
+                    <div className='flex flex-col items-start space-y-[1px] bg-[#A7AEB91A] hover:scale-105'>
+                        <img src={Ha} alt=""
+                            className=' w-full object-cover'
+                        />
+                        <div className=' shadow-md shadow-gray-400 w-full rounded-b-md'>
+                            <div className='flex flex-row justify-between pt-2 px-4'>
+                                <div className='pt-2'>
+                                    <h2 className='font-semibold'>Jay's Menu </h2>
+                                    <div className='flex space-x-2 pt-2'>
+                                        <p> A comprehensive online menu for browsing dishes, exploring descriptions, and viewing visuals</p>
+
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className='flex flex-row justify-between px-4 pt-4 pb-4'>
+                                <a href="https://github.com/abolarin100/food-menu">
+                                    <button className='hover:bg-[#7b8ca71a] hover:scale-125 text-xs bg-[#415f8f1a] text-blue-500 border-1 border-blue-500' >GitHub</button>
+                                </a>
+                                <a href="https://jays-menu.vercel.app/">
+                                    <button className='hover:bg-[#a5b3c91a] hover:scale-125 text-xs bg-[#415f8f1a] text-blue-500 border-1 border-blue-500' >Live link</button>
+
+                                </a>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                    
                     <div className='flex flex-col items-start space-y-[1px] bg-[#A7AEB91A] hover:scale-105  '>
                         <img src={Ja} alt=""
                             className=' w-full object-cover'
